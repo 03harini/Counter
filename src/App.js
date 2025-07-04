c9536c66ee01e43d8165175120bbb969383f158a
Harini P <hariniparameswaran03@gmail.com>
2025-07-04 09:37:07 +0530

    first

--- src/App.js
+++ src/App.js
@@ -2,16 +2,36 @@
 import './App.css';
 import Counter from './counter';
 import React,{ useState } from 'react'
-
+//Two way binding-changes in UI(like from inputs),updates the components state
 function App() {
   const[darkMode,setDarkMode]=useState(false);
+  const[show,setShow]=useState(false); 
+  const[name,setName]=useState("");
+   const toggleText = () => {
+    setShow(!show);
+  };
   return (
+    <div className={show ? "show":"hide"}>
    <div className={darkMode ? "container dark":"container"}>
     <h1>Click Counter App</h1>
     <Counter/>
+    <input type="text" 
+    placeholder="Enter your name" 
+    value={name} 
+    onChange={(e)=>setName(e.target.value)}
+    ></input>
+    <p>Hello,{name}</p>
     <button className="btn" onClick={()=>setDarkMode(!darkMode)}>
       {darkMode ? "Switch to LightMode" :"Switch to darkmode"}
     </button>
+    <button onClick={toggleText}>
+        {show ? 'Hide' : 'show'}
+      </button>
+       {show&&<p style={{ marginTop: '20px', fontSize: '18px' }}>
+          👋 Hello! This is the text you can show or hide.
+        </p>}
+
+    </div>
    </div>
   );
 }
